6d0bcc3623493f0a52988ea6361aea3e10c31196
telin0411 <eggnome0411@gmail.com>
2020-05-17 06:24:26 -0700

    600 samples

--- analyze_qualtrics_raw_binpiqa.sh
+++ analyze_qualtrics_raw_binpiqa.sh
@@ -1,7 +1,6 @@
 python3 analyze_qualtrics_raw.py \
     --input_csv_files "files/BINPIQASurveyTeLinToDistributePart1.csv" \
                       "files/BINPIQASurveyTeLinToDistributePart2.csv" \
-                      "files/BINPIQASurveyTeLinToDistributePart3.csv" \
     --task physicalbinqa \
     --samples_csv ./files/binpiqa_survey_sampels.csv \
     --data_jsonl ./files/bin-dev.jsonl \
@@ -11,3 +10,4 @@ python3 analyze_qualtrics_raw.py \
     # --start_block 22 \
     # --end_block 26 \
     # --input_csv_files "files/BIN-PIQA-Survey-Te-Lin-To-Distribute-Part1_May 4, 2020_15.39.csv" \
+                      # "files/BINPIQASurveyTeLinToDistributePart3.csv" \
