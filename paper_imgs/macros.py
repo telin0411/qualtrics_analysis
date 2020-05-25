3e610c4686586d060d85ea9a433285557d545c26
telin0411 <eggnome0411@gmail.com>
2020-05-24 23:27:47 -0700

    update

--- paper_imgs/macros.py
+++ paper_imgs/macros.py
@@ -39,6 +39,12 @@ IF_COMMON_SENSE = {
 IF_COMMON_SENSE_ID = {v: u for u, v in IF_COMMON_SENSE.items()}
 
 
+OVERALL = {
+    "Performance": 0,
+}
+OVERALL_ID = {v: u for u, v in OVERALL.items()}
+
+
 TASK_ABR = {
     "physicaliqa": "piqa",
     "physicalbinqa": "binpiqa",
@@ -50,10 +56,12 @@ CAT_NAMES = {
     "cat": "Physical Common Sense Categories",
     "com": "If Common Sense",
     "clearness": "Clearness of Questions",
+    "overall": "Overall Performances"
 }
 CAT_ID_DICTS = {
     "edu": EDUCATION_LEVEL_ID,
     "cat": CATEGORIES_ID,
     "com": IF_COMMON_SENSE_ID,
     "clearness": CLEARNESS_ID,
+    "overall": OVERALL_ID,
 }
